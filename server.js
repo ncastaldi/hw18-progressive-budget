c842363434259634af05bfe5ef85d982e29ea0d3
Nathan Castaldi <nathan@castaldi.dev>
2021-02-10 07:48:09 -0500

    formatted file for readability

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
